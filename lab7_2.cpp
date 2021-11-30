91e4105900dc1c2506861e76ff81cc64304342fa
BestChennn <bestchenbestchen@gmail.com>
2021-11-30 17:03:24 +0700

    lab7.2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,12 +2,31 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+
+	int i = 0, P[5000], k, result, q=0;
+	while(q < 5000){
+		P[q] = 2*q+1;
+		q++;
+	}
+
+	cout<<"Please input k: ";
+	cin>> k ;
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+	if(k > 0){
+		while(i<5000){
+			if((P[i]%k)==0){
+				result = result - P[i];
+				}else{
+					result = result + P[i];
+					}
+					i++;
+					}
+		cout<<"Result = "<< result ;	
+		
+			}else{
+		cout<<"Invalid input!!!";
 	}
 	
 	return 0;
